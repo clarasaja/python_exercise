20cc953cee8ff0d7c1c91dbe1d3c2f64b7e64e64
clarasaia <clara.saia.1997@gmail.com>
2025-07-29 10:57:59 +0200

    es4 aggiornato

--- es4_csv_class.py
+++ es4_csv_class.py
@@ -3,6 +3,16 @@ class CSVFile():
     def __init__(self,nomefile):
         self.nomefile = nomefile
 
+        # Provo ad aprirlo e leggere una riga [ho guardato la soluzione :3]
+        self.can_read = True
+        try:
+            file_da_aprire = open(self.nomefile, 'r')
+            file_da_aprire.readline()
+        except Exception as errore:
+            self.can_read = False
+            print('Errore in apertura del file: "{}"'.format(errore))
+
+
     def __str__(self):
         return "Questo è un file CSV: {}".format(self.nomefile)
     
@@ -10,22 +20,30 @@ class CSVFile():
         return f"CSVFile('{self.nomefile}')"
     
     def get_data(self):
-        dati = []
-        with open(self.nomefile,'r') as miofile:
-            for riga in miofile:
-                elementi = riga.split(',')
-                
-                if elementi[0] != 'Date':
-                    data = elementi[0]
-                    valore = float(elementi[1])
-                    dati.append([data,valore])
-        return dati
-    
+        if not self.can_read:
+            print('Errore, file non aperto o illeggibile')
+            return None
+        else:
+            dati = []
+            with open(self.nomefile,'r') as miofile:
+                for riga in miofile:
+                    elementi = riga.split(',')
+                    
+                    if elementi[0] != 'Date':
+                        data = elementi[0]
+                        valore = float(elementi[1])
+                        dati.append([data,valore])
+            return dati
+        
 
 
 ###----Utilizzo---####
 file_dati_vendite = CSVFile('shampoo_sales.csv') #crea un oggetto CSVFile
+file_errato = CSVFile('shampoo_sales.cs') #fa il print dell'excetpion
 
 dati = file_dati_vendite.get_data()
+dati2 = file_errato.get_data()
+
 
 print(dati)
+print(dati2)
