409e4be45ab94e703a3ccd4607d443ec24fd48ba
clarasaia <clara.saia.1997@gmail.com>
2025-07-29 12:13:45 +0200

    prove che non funzionano

--- es4_csv_class.py
+++ es4_csv_class.py
@@ -28,22 +28,31 @@ class CSVFile():
             with open(self.nomefile,'r') as miofile:
                 for riga in miofile:
                     elementi = riga.split(',')
+
+                    # Posso anche pulire il carattere di newline 
+                    # dall'ultimo elemento con la funzione strip():
+                    elementi[-1] = elementi[-1].strip()
+
                     
                     if elementi[0] != 'Date':
-                        data = elementi[0]
-                        valore = float(elementi[1])
-                        dati.append([data,valore])
+                        #avevo fatto così; ma il float lo vuole nell'esericzio 5 :()
+                        #data = elementi[0]
+                        #valore = float(elementi[1])
+                        #dati.append([data,valore])
+                        dati.append(elementi)
             return dati
         
 
 
 ###----Utilizzo---####
-file_dati_vendite = CSVFile('shampoo_sales.csv') #crea un oggetto CSVFile
-file_errato = CSVFile('shampoo_sales.cs') #fa il print dell'excetpion
+prova_il_codice = False
+if prova_il_codice:
+    file_dati_vendite = CSVFile('shampoo_sales.csv') #crea un oggetto CSVFile
+    file_errato = CSVFile('shampoo_sales.cs') #fa il print dell'excetpion
 
-dati = file_dati_vendite.get_data()
-dati2 = file_errato.get_data()
+    dati = file_dati_vendite.get_data()
+    dati2 = file_errato.get_data()
 
 
-print(dati)
-print(dati2)
+    print(dati)
+    print(dati2)
